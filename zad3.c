cda90d84dcd962828fe149c631df6ff22bd0118d
student-repo <repostudent@gmail.com>
2016-11-27 08:56:23 +0100

    List complete

--- zad3.c
+++ zad3.c
@@ -22,6 +22,12 @@ char *removeBeginSpaces(char *str1);
 
 void fillTableCommand(char *tableCommand[], char *stringCommand, int a);
 
+void outputInputRedirection(char *line, int redirectionType, char redirectionChar, char *redirectionStr);
+
+void handleCommonCommand(char *line);
+
+void handlePipe(char *line);
+
 int main(void)
 {
 
@@ -38,208 +44,31 @@ size_t size;
 printf("lsh shell $ ");
 while (getline(&line, &size, stdin)) {
 
-printf("lsh shell $ ");
+  printf("lsh shell $ ");
 
-if(feof(stdin)){
-  exit(1);
-}
-line=strtok(line, "\n");
-if(line == NULL){
-  ;
-}
-else if (line != NULL && strpbrk(line, "|") != 0){
-  char *pipeCmd1 = (char *) malloc((int)strlen(line));
-  char *pipeCmd2 = (char *) malloc((int)strlen(line));
-  const char pipeChar = '|';
-  strcpy(pipeCmd1, line);
-  strcpy(pipeCmd2, line);
-  // printf("%s\n", pipeCmd2 );
-
-  pipeCmd1 = strchr(pipeCmd1, pipeChar);
-memmove(pipeCmd1, pipeCmd1+1, strlen(pipeCmd1));
-int a = strlen(pipeCmd2) - strlen(pipeCmd1) - 1;
-pipeCmd1=removeBeginSpaces(pipeCmd1);
-pipeCmd2[a]='\0';
-
-    int aa =wordsAmount(pipeCmd1);
-    char *command1Array[aa+1];
-    fillTableCommand(command1Array, pipeCmd1, aa);
-
-
-        int aaa =wordsAmount(pipeCmd2);
-
-        char *command2Array[aaa+1];
-        fillTableCommand(command2Array, pipeCmd2, aaa);
-
-int pid, status;
-int fd[2];
-
-  pipe(fd);
-
-  runsource(fd, command2Array);
-  rundest(fd, command1Array);
-
-  close(fd[0]); close(fd[1]);
-
-}///////////////////////////////////////////////////////////////////////
-else if (line != NULL && strstr(line, "2>") != NULL){
-  char *errFile = (char *) malloc((int)strlen(line));
-  char *cmd2 = (char *) malloc((int)strlen(line));
-  int saved_stdout1;
-  const char *errChar = "2>";
-  strcpy(errFile, line);
-  strcpy(cmd2, line);
-  saved_stdout1 = dup(1);
-
-  errFile = strstr(errFile, errChar);
-  memmove(errFile, errFile+2, strlen(errFile));
-  int b = strlen(cmd2) - strlen(errFile) - 2;
-  errFile=removeBeginSpaces(errFile);
-  cmd2[b]='\0';
-
-  // printf("%s\n",errFile );
-  // printf("%s\n",cmd2 );
-
-
-  int m =wordsAmount(cmd2);
-  char *command4Array[m+1];
-  fillTableCommand(command4Array, cmd2, m);
-
-  int newfd1;	/* new file descriptor */
-  if ((newfd1 = open(errFile, O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0) {
-		perror(errFile);	/* open failed */
-		exit(1);
-	}
-  // printf("!%d!\n",newfd );
-
-  dup2(newfd1, 2);
-
-  pid_t  piddd;
-
-  if ((piddd = fork()) < 0) {     /* fork a child process           */
-       printf("*** ERROR: forking child process failed\n");
-       exit(1);
+  if(feof(stdin)){
+    exit(1);
   }
-  else if (piddd == 0) {
-       execvp(*command4Array, command4Array);
-       perror("exec: error");
-       exit(0);
+  line=strtok(line, "\n");
+  if(line == NULL){
+    ;
   }
-  waitpid(piddd, NULL, 0);
-  dup2(saved_stdout1, 1);
-close(saved_stdout1);
-
-}
-else if (line != NULL && strpbrk(line, ">") != 0){/////////////////////////////////////////////////
-  char *inputfile = (char *) malloc((int)strlen(line));
-  char *outputCmd = (char *) malloc((int)strlen(line));
-  int saved_stdout;
-  const char outputChar = '>';
-  strcpy(inputfile, line);
-  strcpy(outputCmd, line);
-  saved_stdout = dup(1);
-
-  inputfile = strchr(inputfile, outputChar);
-  memmove(inputfile, inputfile+1, strlen(inputfile));
-  int a = strlen(outputCmd) - strlen(inputfile) - 1;
-  inputfile=removeBeginSpaces(inputfile);
-  outputCmd[a]='\0';
-
-  int n =wordsAmount(outputCmd);
-  char *command3Array[n+1];
-  fillTableCommand(command3Array, outputCmd, n);
-
-  int newfd;	/* new file descriptor */
-  if ((newfd = open(inputfile, O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0) {
-		perror(inputfile);	/* open failed */
-		exit(1);
-	}
-  // newfd = open(inputfile, O_RDONLY);
-  // printf("!%d!\n",newfd );
-
-  dup2(newfd, 1);
-
-  pid_t  pidd;
-
-  if ((pidd = fork()) < 0) {     /* fork a child process           */
-       printf("*** ERROR: forking child process failed\n");
-       exit(1);
+  else if (line != NULL && strpbrk(line, "|") != 0){
+    handlePipe(line);
   }
-  else if (pidd == 0) {
-       execvp(*command3Array, command3Array);
-       perror("exec: error");
-       exit(0);
+  else if (line != NULL && strstr(line, "2>") != NULL){
+    outputInputRedirection(line, 2, ' ', "2>");
   }
-  waitpid(pidd, NULL, 0);
-  dup2(saved_stdout, 1);
-close(saved_stdout);
-
-}
-else if (line != NULL && strpbrk(line, "<") != 0){/////////////////////////////////////////////////
-  char *inputfile = (char *) malloc((int)strlen(line));
-  char *outputCmd = (char *) malloc((int)strlen(line));
-  int saved_stdout;
-  const char outputChar = '<';
-  strcpy(inputfile, line);
-  strcpy(outputCmd, line);
-  saved_stdout = dup(1);
-
-  inputfile = strchr(inputfile, outputChar);
-  memmove(inputfile, inputfile+1, strlen(inputfile));
-  int a = strlen(outputCmd) - strlen(inputfile) - 1;
-  inputfile=removeBeginSpaces(inputfile);
-  outputCmd[a]='\0';
-
-  int n =wordsAmount(outputCmd);
-  char *command3Array[n+1];
-  fillTableCommand(command3Array, outputCmd, n);
-
-  int newfd;	/* new file descriptor */
-  // if ((newfd = open(inputfile, O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0) {
-	// 	perror(inputfile);	/* open failed */
-	// 	exit(1);
-	// }
-  newfd = open(inputfile, O_RDONLY);
-  // printf("!%d!\n",newfd );
-
-  dup2(newfd, STDIN_FILENO);//1
-
-  pid_t  pidd;
-
-  if ((pidd = fork()) < 0) {     /* fork a child process           */
-       printf("*** ERROR: forking child process failed\n");
-       exit(1);
+  else if (line != NULL && strpbrk(line, ">") != 0){
+    outputInputRedirection(line, 1, '>', "");
   }
-  else if (pidd == 0) {
-       execvp(*command3Array, command3Array);
-       perror("exec: error");
-       exit(0);
+  else if (line != NULL && strpbrk(line, "<") != 0){
+    outputInputRedirection(line, 0, '<', "");
   }
-  waitpid(pidd, NULL, 0);
-  dup2(saved_stdout, STDIN_FILENO);
-close(saved_stdout);
-close(newfd);
-
+  else{
+    handleCommonCommand(line);
+  }
 }
-else{
-  int runInBackground = etAtTheEnd(line);
-
-  line = removeEtFromCommand(line, runInBackground);
-
-  int a =wordsAmount(line);
-
-  char *commandArray[a+1];
-  fillTableCommand(commandArray, line, a);
-
-      if(*commandArray != NULL && strcmp("exit",commandArray[0]) == 0 ){
-        exit(1);
-      }
-
-    execute(commandArray, runInBackground);
-}
-
-}
-
   return 0;
 }
 
@@ -307,11 +136,7 @@ char *removeEtFromCommand(char *command, int runInBackground){
   }
   command[strlen(command) - 1] = '\0';
   return command;
-
-
 }
-
-
 void
 rundest(int pfd[], char **cmm2)	/* run the second part of the pipeline, cmd2 */
 {
@@ -329,12 +154,8 @@ rundest(int pfd[], char **cmm2)	/* run the second part of the pipeline, cmd2 */
    		perror(cmm2[0]);/* it failed! */
       waitpid(pid, NULL, 0);
        exit(0);
-
   }
-
-
 }
-
 
 void
 runsource(int pfd[], char **cmm1)	/* run the first part of the pipeline, cmd1 */
@@ -353,8 +174,6 @@ runsource(int pfd[], char **cmm1)	/* run the first part of the pipeline, cmd1 */
    perror(cmm1[0]);	/* it failed! */
    waitpid(pid, NULL, 0);
        exit(0);
-
-
   }
 }
 
@@ -377,4 +196,110 @@ void fillTableCommand(char *tableCommand[], char *stringCommand, int a){
           p = strtok (NULL, " ");
       }
       tableCommand[a]=NULL;
+    }
+void outputInputRedirection(char *line, int redirectionType, char redirectionChar, char *redirectionStr){
+  char *secCmdElem = (char *) malloc((int)strlen(line));
+  char *firstCmdElem = (char *) malloc((int)strlen(line));
+  int saved_stdout;
+  strcpy(secCmdElem, line);
+  strcpy(firstCmdElem, line);
+  saved_stdout = dup(1);
+  int a;
+
+if(redirectionType == 2){
+  secCmdElem = strstr(secCmdElem, redirectionStr);
+  memmove(secCmdElem, secCmdElem+2, strlen(secCmdElem));
+  a = strlen(firstCmdElem) - strlen(secCmdElem) - 2;
+}
+else{
+  secCmdElem = strchr(secCmdElem, redirectionChar);
+  memmove(secCmdElem, secCmdElem+1, strlen(secCmdElem));
+  a = strlen(firstCmdElem) - strlen(secCmdElem) - 1;
+}
+  secCmdElem=removeBeginSpaces(secCmdElem);
+  firstCmdElem[a]='\0';
+
+  int n =wordsAmount(firstCmdElem);
+  char *command3Array[n+1];
+  fillTableCommand(command3Array, firstCmdElem, n);
+
+  int newfd;	/* new file descriptor */
+  if(redirectionType == 0){
+    newfd = open(secCmdElem, O_RDONLY);
+  }
+  else{
+    if ((newfd = open(secCmdElem, O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0) {
+  		perror(secCmdElem);	/* open failed */
+  		exit(1);
+  	}
+  }
+
+  dup2(newfd, redirectionType);
+
+  pid_t  pidd;
+
+  if ((pidd = fork()) < 0) {     /* fork a child process           */
+       printf("*** ERROR: forking child process failed\n");
+       exit(1);
+  }
+  else if (pidd == 0) {
+       execvp(*command3Array, command3Array);
+       perror("exec: error");
+       exit(0);
+  }
+  waitpid(pidd, NULL, 0);
+  dup2(saved_stdout, redirectionType);
+close(saved_stdout);
+}
+
+void handleCommonCommand(char *line){
+  int runInBackground = etAtTheEnd(line);
+
+  line = removeEtFromCommand(line, runInBackground);
+
+  int a =wordsAmount(line);
+
+  char *commandArray[a+1];
+  fillTableCommand(commandArray, line, a);
+
+      if(*commandArray != NULL && strcmp("exit",commandArray[0]) == 0 ){
+        exit(1);
+      }
+
+    execute(commandArray, runInBackground);
+}
+
+void handlePipe(char *line){
+  char *pipeCmd1 = (char *) malloc((int)strlen(line));
+  char *pipeCmd2 = (char *) malloc((int)strlen(line));
+  const char pipeChar = '|';
+  strcpy(pipeCmd1, line);
+  strcpy(pipeCmd2, line);
+
+  pipeCmd1 = strchr(pipeCmd1, pipeChar);
+memmove(pipeCmd1, pipeCmd1+1, strlen(pipeCmd1));
+int a = strlen(pipeCmd2) - strlen(pipeCmd1) - 1;
+pipeCmd1=removeBeginSpaces(pipeCmd1);
+pipeCmd2[a]='\0';
+
+    int aa =wordsAmount(pipeCmd1);
+    char *commandArray[aa+1];
+    fillTableCommand(commandArray, pipeCmd1, aa);
+
+
+        int aaa =wordsAmount(pipeCmd2);
+
+        char *command2Array[aaa+1];
+        fillTableCommand(command2Array, pipeCmd2, aaa);
+
+int pid, status;
+int fd[2];
+
+  pipe(fd);
+
+  runsource(fd, command2Array);
+  rundest(fd, commandArray);
+
+  close(fd[0]); close(fd[1]);
+
 }
